d5b2c1c42ad62239c3c270064766a277c232a119
Ivan <bomak54@gmail.com>
2021-08-09 12:26:50 +0300

    added library express and created get metod method

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
-const PORT = process.env.PORT || 300
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+})
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server Start ${PORT}`)
 })
